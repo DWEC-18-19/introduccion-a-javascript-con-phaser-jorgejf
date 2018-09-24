248232d25d65a81dbf492a5e9925ccefb31a31eb
JorgeJF <jorgejimenezfdez@gmail.com>
2018-09-24 16:50:25 +0200

    version_final

--- game.js
+++ game.js
@@ -10,18 +10,39 @@ var text;
 var winningMessage;
 var won = false;
 var currentScore = 0;
-var winningScore = 100;
+var winningScore = 50;
 
 // add collectable items to the game
 function addItems() {
   items = game.add.physicsGroup();
-  createItem(375, 300, 'coin');
+  createItem(150, 450, 'coin');
+  createItem(550, 450, 'coin');
+  createItem(350, 350, 'coin');
+  createItem(500, 250, 'coin');
+  createItem(700, 200, 'coin');
+  createItem(150, 200, 'coin');
+  createItem(250, 150, 'coin');
+  createItem(550, 100, 'coin');
+  createItem(200, 50, 'coin');
+  createItem(370, 500, 'poison');
+  createItem(100, 375, 'poison');
+  createItem(125, 50, 'star');
+  createItem(800, 600, 'fondo');
+
 }
 
 // add platforms to the game
 function addPlatforms() {
   platforms = game.add.physicsGroup();
-  platforms.create(450, 150, 'platform');
+  platforms.create(100, 500, 'platform');
+  platforms.create(500, 500, 'platform');
+  platforms.create(300, 400, 'platform_2');
+  platforms.create(450, 300, 'platform_2');
+  platforms.create(650, 250, 'platform');
+  platforms.create(100, 250, 'platform');
+  platforms.create(200, 200, 'platform');
+  platforms.create(500, 150, 'platform_2');
+  platforms.create(150, 100, 'platform_2');
   platforms.setAll('body.immovable', true);
 }
 
@@ -43,7 +64,11 @@ function createBadge() {
 // when the player collects an item on the screen
 function itemHandler(player, item) {
   item.kill();
-  currentScore = currentScore + 10;
+  if(item.key=='poison'){
+    currentScore=currentScore-10;
+  }else{
+    currentScore = currentScore + 10;
+  }
   if (currentScore === winningScore) {
       createBadge();
   }
@@ -61,14 +86,22 @@ window.onload = function () {
   
   // before the game begins
   function preload() {
-    game.stage.backgroundColor = '#5db1ad';
+ game.stage.backgroundColor = '#555555';
+
     
     //Load images
     game.load.image('platform', 'platform_1.png');
+    game.load.image('platform_2', 'platform_2.png');
+    game.load.image('poison', 'poison.png');
+    game.load.image('star', 'star.png');
+    game.load.image('fondo', 'fondo.jpg');
+    game.load.image('fondo', 'fondo.jpg', 800, 600);
     
     //Load spritesheets
-    game.load.spritesheet('player', 'chalkers.png', 48, 62);
+    game.load.spritesheet('player', 'ash.png', 40, 57);
     game.load.spritesheet('coin', 'coin.png', 36, 44);
+    game.load.spritesheet('poison', 'poison.png', 32, 32);
+    game.load.spritesheet('star', 'star.png', 32, 32);
     game.load.spritesheet('badge', 'badge.png', 42, 54);
   }
 
